18fb39076c7d682914c6a1a6f845b20bc4260d3d
Pro-AmalTarek <30780153+Pro-AmalTarek@users.noreply.github.com>
2018-05-01 15:01:17 +0200

    Edit Dataset

--- Core/DatasetHandle.py
+++ Core/DatasetHandle.py
@@ -1,10 +1,3 @@
-import string
-import io
-import re
-from nltk.collocations import BigramCollocationFinder
-import numpy as np
-from nltk.classify import NaiveBayesClassifier
-
 
 class DatasetHandle:
     datasets = {}
@@ -15,7 +8,12 @@ class DatasetHandle:
         self.datasets["English"] = self.rootDir + "English.txt"
         self.datasets["German"] = self.rootDir + "German.txt"
         self.datasets["French"] = self.rootDir + "French.txt"
-        # self.datasets["EgyptionArabic"] = self.rootDir + "EgyptionArabic.txt"
+        self.datasets["Italian"] = self.rootDir + "Italian.txt"
+        self.datasets["Japanese"] = self.rootDir + "Japanese.txt"
+        self.datasets["Dutch"] = self.rootDir + "Dutch.txt"
+        self.datasets["Hindi"] = self.rootDir + "Hindi.txt"
+        self.datasets["Spanish"] = self.rootDir + "Spanish.txt"
+        self.datasets["EgyptionArabic"] = self.rootDir + "EgyptionArabic.txt"
 
     def read_datasets(self, file):
         text = open(file, 'r', encoding='utf-8').read()
